e1035fe2a53400e3b9a03fbc340dff950ba8e6df
Brian <namedtuple@gmail.com>
2014-07-17 18:32:32 -0700

    Initial development, 14

--- main.py
+++ main.py
@@ -3,15 +3,18 @@ from functools import partial
 from itertools import combinations
 from random import choice as PICK
 from random import uniform as RND
+import sys
 from threading import Timer
 from tkinter import N, S, E, W
 import tkinter
 import calculations
 from coordinate import Coordinate, Oval
+from random_seeds import BallOptionsSeeded
+import random_seeds
 from tk_colors import all_colors as colors
-WindowOptions = namedtuple('WindowOptions',       'bg, fps, trace_xy, trace_hits')
-BallOptions = namedtuple('BallOptions',         'N, XY, R, V')
-CollisionOptions = namedtuple('CollisionOptions',    'trace')
+WindowOptions = namedtuple('WindowOptions', 'bg, fps, trace_xy, trace_hits, trace_n')
+BallOptionsRandom = namedtuple('BallOptionsRandom', 'N, XY, R, V')
+CollisionOptions = namedtuple('CollisionOptions', 'trace')
 class Game:
     def __init__(self, window_options, ball_options, collision_options):
         self.window_options = window_options
@@ -20,9 +23,9 @@ class Game:
         self.game_window = Window(self.window_options)
         self.balls = Balls(self.ball_options)
         self.collisions = Collisions(self.balls.ball_list, self.collision_options)
-        self.collisions2 = Collisions2()
+        self.collision_handler = CollisionHandler(self.balls.ball_list)
     def GAME_LOOP(self):
-        ball_list = CollisionHandler().handle_any_collisions(self.balls.ball_list, self.collisions, self.collisions2)
+        ball_list = self.collision_handler(self.balls.ball_list, self.collisions)
         ball_list = self.balls.advance_ball_positions(ball_list)
         self.game_window.DRAW_BALLS(ball_list)
         self.game_window.root_window.after(self.game_window._mSPF, self.GAME_LOOP)
@@ -39,6 +42,7 @@ class Window:
         self._FPS = options.fps
         self._trace_xy = options.trace_xy
         self._trace_hits = options.trace_hits
+        self._trace_n = options.trace_n
         self._mSPF = calculations.mSPF_from_FPS(self._FPS)
         self._CLICK_PAUSE = False
         self._RESIZE_PAUSE = False
@@ -49,8 +53,8 @@ class Window:
         self.root_window.columnconfigure(index=0, weight=1)
         self.root_window.rowconfigure(index=0, weight=1)
     def _SETUP_controls(self):
-        self.root_window.bind('<Configure>',   self._CTRL_on_configure)
-        self.root_window.bind('<Button-1>',    self._CTRL_on_mouse_click_left)
+        self.root_window.bind('<Configure>', self._CTRL_on_configure)
+        self.root_window.bind('<Button-1>', self._CTRL_on_mouse_click_left)
     def _CTRL_on_configure(self, event):
         self._RESIZE_PAUSE = True
         t = Timer(0.5, self._CTRL_on_configure_set_resize_pause)
@@ -73,13 +77,14 @@ class Window:
                     self._trace_ball_position(ball, len(ball_list) - 1)
                 if self._trace_hits:
                     self._trace_number_hits(ball)
+                if self._trace_n:
+                    self._trace_ball_number(ball)
             self.root_window.update_idletasks()
     def _draw_ball(self, ball):
         X1, Y1, X2, Y2 = Oval(ball.position, ball.radius).all()
         X1, Y1 = Coordinate((X1, Y1)).absolute(self._GET_WH())
         X2, Y2 = Coordinate((X2, Y2)).absolute(self._GET_WH())
         self._canvas.create_oval(X1, Y1, X2, Y2, fill=ball.color)
-        self._canvas.create_arc(X1, Y1, X2, Y2)
     def _trace_ball_position(self, ball, num_balls):
         x, y = ball.position
         print('{:20} : ( {:.2f} , {:.2f} )'.format(ball.color, x, y))
@@ -89,14 +94,16 @@ class Window:
         X0, Y0 = Coordinate(ball.position).absolute(self._GET_WH())
         total_hits = ball.total_hits
         self._canvas.create_text(X0, Y0, text=total_hits)
+        pass
+    def _trace_ball_number(self, ball):
+        X0, Y0 = Coordinate(ball.position).absolute(self._GET_WH())
+        self._canvas.create_text(X0, Y0, text=ball.n)
     def _draw_arc(self, ball):
         pass
 class Balls:
     def __init__(self, options):
         self.N = options.N
-        self.xy,    self.XY = options.XY
-        self.r,     self.R = options.R
-        self.v,     self.V = options.V
+        self.options = options
         self.ball_list = self.__SETUP__balls()
     def __SETUP__balls(self):
         ball_list = []
@@ -110,13 +117,27 @@ class Balls:
             ball_list.append(new_ball)
         return ball_list
     def _create_random_ball(self, n):
-        X = RND(self.xy, self.XY)
-        Y = RND(self.xy, self.XY)
-        R = RND(self.r, self.R)
-        V = RND(self.v, self.V)
-        V = PICK([V, V * -1])
-        C = PICK(colors)
-        return Ball(number=n, center=(X, Y), radius=(R, R), velocity=(V, V), color=C)
+        print(type(self.options))
+        print(type(self.options) == BallOptionsRandom)
+        print(type(self.options) == BallOptionsSeeded)
+        if type(self.options) == BallOptionsRandom:
+            xy, XY = self.options.XY
+            r, R = self.options.R
+            v, V = self.options.V
+            X = RND(xy, XY)
+            Y = RND(xy, XY)
+            R = RND(r, R)
+            V = RND(v, V)
+            V = PICK([V, V * -1])
+            C = PICK(colors)
+            return Ball(number=n, center=(X, Y), radius=(R, R), velocity=(V, V), color=C)
+        elif type(self.options) == BallOptionsSeeded:
+            X = self.options.X[n]
+            Y = self.options.Y[n]
+            R = self.options.R[n]
+            V = self.options.V[n]
+            C = self.options.C[n]
+            return Ball(number=n, center=(X, Y), radius=(R, R), velocity=(V, V), color=C)
     def _new_ball_overlaps_with_existing_ball(self, new_ball, ball_list):
         for ball in ball_list:
             overlapping = CollisionHandler._ball_collision(self, new_ball, ball)
@@ -175,12 +196,59 @@ class Ball:
         self.number_hit = 1
         self.total_hits += 1
 class CollisionHandler:
-    def handle_any_collisions(self, ball_list, collisions, collisions2):
-        collisions2.reset_this_frame_list()
+    def __init__(self, ball_list):
+        self.list_hits_last_50_frames = [[] for i in range(0, 50)]
+        self.counted_hits_last_50_frames = {}
+        self.hits_this_frame = []
+        self.threshold = 5
+        self.ball_color_list = self._get_ball_color_list(ball_list)
+    def _get_ball_color_list(self, ball_list):
+        li = []
+        for b in ball_list:
+            li.append(b.color)
+        return li
+    def __call__(self, ball_list, collisions):
+        self._reset()
+        self._count_number_of_same_hits_in_last_50_frames()
+        self._count_all_number_same_hits_in_last_50_frames_above_threshold()
+        self._change_ball_color_if_above_threshold(ball_list)
         ball_list = self._handle_wall_collision(ball_list)
-        ball_list = self._handle_ball_collision(ball_list, collisions, collisions2)
-        collisions2.update_last_50_frames_lists()
+        ball_list = self._handle_ball_collision(ball_list, collisions)
+        self._update_hits_for_last_50_frames_list()
         return ball_list
+    def _reset(self):
+        self.hits_this_frame = []
+        del self.counted_hits_last_50_frames
+        self.counted_hits_last_50_frames = {}
+    def _count_number_of_same_hits_in_last_50_frames(self):
+        count_di = {}
+        for hit_list in self.list_hits_last_50_frames:
+            for balltuple in hit_list:
+                if balltuple not in count_di.keys():
+                    count_di[balltuple] = 1
+                elif balltuple in count_di.keys():
+                    count_di[balltuple] += 1
+        self.counted_hits_last_50_frames = count_di
+        del count_di
+    def _count_all_number_same_hits_in_last_50_frames_above_threshold(self):
+        li = []
+        for ij in self.counted_hits_last_50_frames.keys():
+            if self.counted_hits_last_50_frames[ij] > self.threshold:
+                li.append(ij)
+        self.above_threshold_this_frame = li
+        print(self.above_threshold_this_frame)
+    def _change_ball_color_if_above_threshold(self, ball_list):
+        uniques = set()
+        for i, j in self.above_threshold_this_frame:
+            uniques.add(i)
+            uniques.add(j)
+        for ball in ball_list:
+            if ball.n in uniques:
+                ball.color = 'yellow'
+                print('!')
+            else:
+                ball.color = self.ball_color_list[ball.n]
+        del uniques
     def _handle_wall_collision(self, ball_list):
         for i in range(0, len(ball_list)):
             ball = ball_list[i]
@@ -197,22 +265,15 @@ class CollisionHandler:
             ball.velocity = (dx, dy)
             ball_list[i] = ball
         return ball_list
-    def _handle_ball_collision(self, ball_list, collisions, collisions2):
+    def _handle_ball_collision(self, ball_list, collisions):
         index_combo_list = list(combinations(range(0, len(ball_list)), 2))
         for i, j in index_combo_list:
             ball_A, ball_B = ball_list[i], ball_list[j]
             DX, DY = ball_A.velocity
             dx, dy = ball_B.velocity
             if self._ball_collision(ball_A, ball_B):
-                collisions2.add_to_collisions_this_frame(ball_A, ball_B)
-                self._add_to_collisions_this_frame(ball_A, ball_B)
-                if collisions.trace:
-                    self._trace_collisions(ball_A, ball_B)
-                ball_A, ball_B = self._update_last_hits(ball_A, ball_B)
-                if self._blacklist(ball_A, ball_B, collisions):
-                    if collisions.trace:
-                        self._trace_blacklisted_collisions(ball_A, ball_B)
-                elif not self._blacklist(ball_A, ball_B, collisions):
+                self._add_to_hits_this_frame_li(ball_A, ball_B)
+                if not self._above_threshold_this_frame(ball_A, ball_B):
                     DXDY, dxdy = calculations.new_velocity(ball_A, ball_B)
                     DX, DY = DXDY
                     dx, dy = dxdy
@@ -232,46 +293,17 @@ class CollisionHandler:
             return True
         else:
             return False
-    def _update_last_hits(self, ball_A, ball_B):
-        if ball_A.last_hit == ball_B:
-            ball_A.increment_last_hit()
-        else:
-            ball_A.set_new_last_hit(ball_B)
-        if ball_B.last_hit == ball_A:
-            ball_B.increment_last_hit()
-        else:
-            ball_B.set_new_last_hit(ball_A)
-        return (ball_A, ball_B)
-    def _blacklist(self, ball_A, ball_B, collisions):
+    def _add_to_hits_this_frame_li(self, ball_A, ball_B):
         ij = calculations.ball_order(ball_A, ball_B)
-        if ball_A.number_hit > 10 and ball_B.number_hit > 10:
-            collisions.set(ij, True)
-            t = Timer(1, partial(collisions.set, ij, False))
-            t.start()
-        return collisions.get(ij)
-    def _trace_collisions(self, ball_A, ball_B):
-        last_hit_for_A = '{:2}-{:20}'.format(str(ball_A.last_hit.n), ball_A.last_hit.color)
-        last_hit_for_B = '{:2}-{:20}'.format(str(ball_B.last_hit.n), ball_B.last_hit.color)
-        A = 'A: {:2}-{:20} Last Hit: {} Total Hits: {}'.format(ball_A.n, ball_A.color, last_hit_for_A, ball_A.number_hit)
-        B = 'B: {:2}-{:20} Last Hit: {} Total Hits: {}'.format(ball_B.n, ball_B.color, last_hit_for_B, ball_B.number_hit)
-        print(A, B, sep='\t')
-    def _trace_blacklisted_collisions(self, ball_A, ball_B):
-        A = '{}-{}'.format(ball_A.n, ball_A.color)
-        B = '{}-{}'.format(ball_B.n, ball_B.color)
-        star = '*' * 20
-        print('{} BREAKING {} and {}!! {}'.format(star, A, B, star))
-class Collisions2:
-    def __init__(self):
-        self.collisions_last_50_frames = [[] for i in range(0, 50)]
-    def reset_this_frame_list(self):
-        self._collisions_this_frame = []
-    def update_last_50_frames_lists(self):
-        old_list = self.collisions_last_50_frames
-        new_list = old_list[1::] + [self._collisions_this_frame]
-        self.collisions_last_50_frames = new_list
-    def add_to_collisions_this_frame(self, ball_A, ball_B):
+        self.hits_this_frame.append(ij)
+    def _above_threshold_this_frame(self, ball_A, ball_B):
         ij = calculations.ball_order(ball_A, ball_B)
-        self._collisions_this_frame.append(ij)
+        if ij in self.above_threshold_this_frame:
+            return True
+        return False
+    def _update_hits_for_last_50_frames_list(self):
+        if len(self.hits_this_frame) > 0:
+            self.list_hits_last_50_frames = self.list_hits_last_50_frames[1::] + [self.hits_this_frame]
 class Collisions:
     def __init__(self, ball_list, options):
         self.trace = options.trace
@@ -288,8 +320,8 @@ class Collisions:
     def set(self, ij, TF):
         self.di[ij] = TF
 if __name__ == '__main__':
-    window_options = WindowOptions(bg='grey', fps=100, trace_xy=False, trace_hits=True)
-    ball_options = BallOptions(N=25, XY=(0.01, 0.99), R=(0.03, 0.05), V=(0.0005, 0.0075))
+    window_options = WindowOptions(bg='grey', fps=100, trace_xy=False, trace_hits=False, trace_n=True)
+    ball_options = BallOptionsRandom(N=25, XY=(0.01, 0.99), R=(0.03, 0.05), V=(0.0005, 0.0075))
     collision_options = CollisionOptions(trace=False)
     game = Game(window_options, ball_options, collision_options)
     game.START()
