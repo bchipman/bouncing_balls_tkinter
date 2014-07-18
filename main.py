f632bbf158effcd2848674f1de3d50ccab6aa056
Brian <namedtuple@gmail.com>
2014-07-17 18:27:54 -0700

    Initial development, 04

--- main.py
+++ main.py
@@ -12,8 +12,22 @@ class Ball:
         self.radius_xy = radius
         self.velocity_xy = velocity
         self.color = color
+        self.recent_wall_collision = False
+        self.recent_ball_collision = False
     def set_color(self, color):
         self.color = color
+    def start_wall_collision_timer(self):
+        self.recent_wall_collision = True
+        wall_timer = Timer(0.1, self.reset_recent_wall_collision)
+        wall_timer.start()
+    def reset_recent_wall_collision(self):
+        self.recent_wall_collision = False
+    def start_ball_collision_timer(self):
+        self.recent_ball_collision = True
+        ball_timer = Timer(25 / 1000, self.reset_recent_ball_collision)
+        ball_timer.start()
+    def reset_recent_ball_collision(self):
+        self.recent_ball_collision = False
 class Game:
     def __init__(self):
         self.__SETUP__variables()
@@ -28,7 +42,7 @@ class Game:
         self._CLICK_PAUSE = False
         self._RESIZE_PAUSE = False
     def __SETUP__balls(self):
-        num_balls = 5
+        num_balls = 30
         ball_list = []
         for n in range(0, num_balls):
             while True:
@@ -40,7 +54,7 @@ class Game:
         self.balls = ball_list
     def _check_if_new_ball_overlapping_with_existing_balls(self, new_ball, ball_list):
         for ball in ball_list:
-            overlapping = self._check_for_ball_ball_collision(new_ball, ball)
+            overlapping = self._ball_ball_collision(new_ball, ball)
             if overlapping:
                 return True
         return False
@@ -58,8 +72,8 @@ class Game:
         self._root_window.columnconfigure(index=0, weight=1)
         self._root_window.rowconfigure(index=0, weight=1)
     def __SETUP__controls(self):
-        self._root_window.bind('<Configure>', self._on_configure)
-        self._root_window.bind('<Button-1>', self._on_mouse_click)
+        self._root_window.bind('<Configure>',   self._on_configure)
+        self._root_window.bind('<Button-1>',    self._on_mouse_click)
     def _trace_ball_position(self):
         for ball in self.balls:
             x, y = ball.center_xy
@@ -68,9 +82,9 @@ class Game:
     def GO(self):
         if not self._CLICK_PAUSE and not self._RESIZE_PAUSE:
             self._canvas.delete(tkinter.ALL)
+            self._ball_WALL_collision_main()
+            self._ball_BALL_collision_main()
             self._change_ball_position()
-            self._change_ball_direction_if_ball_WALL_collision()
-            self._change_ball_direction_if_ball_BALL_collision()
             self._draw_balls()
             self._trace_ball_position()
             self._root_window.update_idletasks()
@@ -86,41 +100,49 @@ class Game:
             x, y = ball.center_xy
             dx, dy = ball.velocity_xy
             ball.center_xy = (x + dx, y + dy)
-    def _change_ball_direction_if_ball_WALL_collision(self):
+    def _ball_WALL_collision_main(self):
         for i in range(0, len(self.balls)):
             ball = self.balls[i]
             x, y = ball.center_xy
             rx, ry = ball.radius_xy
             dx, dy = ball.velocity_xy
-            if self._check_for_ball_wall_collision(x, rx):
-                dx *= -1
-            if self._check_for_ball_wall_collision(y, ry):
-                dy *= -1
+            if self._ball_wall_collision(x, rx):
+                if self._recent_wall_collision(ball) == False:
+                    dx *= -1
+                    ball.start_wall_collision_timer()
+            if self._ball_wall_collision(y, ry):
+                if self._recent_wall_collision(ball) == False:
+                    dy *= -1
+                    ball.start_wall_collision_timer()
             ball.velocity_xy = (dx, dy)
             self.balls[i] = ball
-    def _check_for_ball_wall_collision(self, z, rz):
+    def _ball_wall_collision(self, z, rz):
         if z - rz < 0:
             return True
         elif z + rz > 1:
             return True
         else:
             return False
-    def _change_ball_direction_if_ball_BALL_collision(self):
+    def _recent_wall_collision(self, ball):
+        return ball.recent_wall_collision
+    def _ball_BALL_collision_main(self):
         index_combo_list = list(combinations(range(0, len(self.balls)), 2))
         for i, j in index_combo_list:
             ball_A = self.balls[i]
             ball_B = self.balls[j]
             DX, DY = ball_A.velocity_xy
             dx, dy = ball_B.velocity_xy
-            collision = self._check_for_ball_ball_collision(ball_A, ball_B)
+            collision = self._ball_ball_collision(ball_A, ball_B)
             if collision:
-                DX, DY = DX * -1, DY * -1
-                dx, dy = dx * -1, dy * -1
+                if self._recent_ball_collision(ball_A) == False:
+                    DX, DY = DX * -1, DY * -1
+                if self._recent_ball_collision(ball_B) == False:
+                    dx, dy = dx * -1, dy * -1
             ball_A.velocity_xy = DX, DY
             ball_B.velocity_xy = dx, dy
             self.balls[i] = ball_A
             self.balls[j] = ball_B
-    def _check_for_ball_ball_collision(self, ball_A, ball_B):
+    def _ball_ball_collision(self, ball_A, ball_B):
         X, Y = ball_A.center_xy
         R = ball_A.radius_xy[0]
         x, y = ball_B.center_xy
@@ -131,6 +153,22 @@ class Game:
             return True
         else:
             return False
+    def _recent_ball_collision(self, ball):
+        return ball.recent_ball_collision
+    def _random_direction_change_for_ball_ball_collision(self, DXDYdxdy):
+        DX, DY, dx, dy = DXDYdxdy
+        choices = ['XY', 'XY', 'XY']
+        choice = PICK(choices)
+        if choice == 'X':
+            DX, dx = DX * -1, dx * -1
+            DY, dy = DY,    dy
+        elif choice == 'Y':
+            DX, dx = DX,    dx
+            DY, dy = DY * -1, dy * -1
+        elif choice == 'XY':
+            DX, dx = DX * -1, dx * -1
+            DY, dy = DY * -1, dy * -1
+        return (DX, DY, dx, dy)
     def _get_WH(self):
         return (self._root_window.winfo_width(), self._root_window.winfo_height())
     def _on_configure(self, event):
