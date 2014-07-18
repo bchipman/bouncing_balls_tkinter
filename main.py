4f52575802fa43ce49b52abe8c7d5b927f775134
Brian <namedtuple@gmail.com>
2014-07-17 18:28:57 -0700

    Initial development, 09

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from itertools import combinations
 from tk_colors import select_colors
 from functools import partial
 from coordinate import Coordinate, Oval
+from math import sqrt
 class View:
     def __init__(self):
         self.__SETUP__variables()
@@ -30,9 +31,9 @@ class View:
         self._root_window.bind('<Button-1>',    self.__CTRL__on_mouse_click_left)
     def __CTRL__on_configure(self, event):
         self._RESIZE_PAUSE = True
-        t = Timer(0.5, self._set_resizing_to_false)
+        t = Timer(0.5, self.__CTRL__on_configure___set_resize_pause)
         t.start()
-    def _set_resizing_to_false(self):
+    def __CTRL__on_configure___set_resize_pause(self):
         self._RESIZE_PAUSE = False
     def __CTRL__on_mouse_click_left(self, event):
         if self._CLICK_PAUSE == True:
@@ -65,41 +66,25 @@ class View:
         self._root_window.mainloop()
 class Model:
     def __init__(self):
-        self.__SETUP__model_variables()
+        self._num_balls = 25
         self.__SETUP__balls()
-        self.__SETUP__collision_timer_dictionary()
-        self.__SETUP__add_ball_wall_entries_to_collision_timer_dictionary()
-    def __SETUP__model_variables(self):
-        self.num_balls = 25
+        self.__SETUP__collision_dictionary()
     def __SETUP__balls(self):
         ball_list = []
-        for n in range(0, self.num_balls):
+        for n in range(0, self._num_balls):
             while True:
                 new_ball = self.__SETUP__balls__make_random_ball(n)
-                new_ball_OK = not self.__SETUP__balls__check_if_new_ball_overlapping_with_existing_balls(new_ball, ball_list)
-                if new_ball_OK:
+                new_ball_XY_OK = not self.__SETUP__balls__check_if_new_ball_overlapping_with_existing_balls(new_ball, ball_list)
+                new_ball_color_OK = not self.__SETUP__balls__check_if_new_ball_same_color_as_an_existing_balls(new_ball, ball_list)
+                if new_ball_XY_OK and new_ball_color_OK:
                     break
             ball_list.append(new_ball)
         self.balls = ball_list
-    def __SETUP__collision_timer_dictionary(self):
-        index_combo_list = list(combinations(range(0, len(self.balls)), 2))
-        ball_timers_dict = {}
-        for index_combo in index_combo_list:
-            recent_collision = False
-            ball_timers_dict[index_combo] = recent_collision
-        self._ball_timers_dict = ball_timers_dict
-    def __SETUP__add_ball_wall_entries_to_collision_timer_dictionary(self):
-        di_copy = deepcopy(self._ball_timers_dict)
-        del self._ball_timers_dict
-        for n in range(0, len(self.balls)):
-            di_copy[(n, 'NS')] = False
-            di_copy[(n, 'EW')] = False
-        self._ball_timers_dict = deepcopy(di_copy)
-        del di_copy
     def __SETUP__balls__make_random_ball(self, n):
         XY = RND(0.1, 0.9), RND(0.1, 0.9)
         R = RND(0.03, 0.05)
         V = RND(15, 35) / 10000
+        V = PICK([V, V * -1])
         C = PICK(select_colors)
         return Ball(number=n, center=XY, radius=(R, R), velocity=(V, V), color=C)
     def __SETUP__balls__check_if_new_ball_overlapping_with_existing_balls(self, new_ball, ball_list):
@@ -108,12 +93,24 @@ class Model:
             if overlapping:
                 return True
         return False
+    def __SETUP__balls__check_if_new_ball_same_color_as_an_existing_balls(self, new_ball, ball_list):
+        for ball in ball_list:
+            if ball.color == new_ball.color:
+                return True
+        return False
+    def __SETUP__collision_dictionary(self):
+        index_combo_list = list(combinations(range(0, len(self.balls)), 2))
+        ball_collision_dict = {}
+        for index_combo in index_combo_list:
+            currently_blacklisted = False
+            ball_collision_dict[index_combo] = currently_blacklisted
+        self._ball_collision_dict = ball_collision_dict
     def _change_position_of_balls(self):
         for ball in self.balls:
             ball.move()
     def MOVE(self):
         self._handle_wall_collision()
-        self._ball_BALL_collision_main()
+        self._handle_ball_collision()
         self._change_position_of_balls()
         return self.balls
     def _handle_wall_collision(self):
@@ -140,31 +137,46 @@ class Model:
             dx, dy = ball_B.velocity_xy
             collision = self._ball_ball_collision(ball_A, ball_B)
             if collision:
-                DX, DY = DX * -1, DY * -1
-                dx, dy = dx * -1, dy * -1
+                ball_A, ball_B = self._update_last_hits(ball_A, ball_B)
+                blacklisted = self._blacklist(ball_A, ball_B)
+                if not blacklisted:
+                    DXDY, dxdy = self.new_velocity(ball_A, ball_B)
+                    DX, DY = DXDY
+                    dx, dy = dxdy
             ball_A.velocity_xy = DX, DY
             ball_B.velocity_xy = dx, dy
             self.balls[i] = ball_A
             self.balls[j] = ball_B
-    def _ball_BALL_collision_main(self):
-        index_combo_list = list(combinations(range(0, len(self.balls)), 2))
-        for i, j in index_combo_list:
-            ball_A = self.balls[i]
-            ball_B = self.balls[j]
-            DX, DY = ball_A.velocity_xy
-            dx, dy = ball_B.velocity_xy
-            collision = self._ball_ball_collision(ball_A, ball_B)
-            if collision:
-                if self._recent_ball_collision((i, j)) == False:
-                    self._set_ball_collision_timer_bool_to_True((i, j))
-                    timer = Timer(3, partial(self._set_ball_collision_timer_bool_to_False, ij=(i, j)))
-                    timer.start()
-                    DX, DY = DX * -1, DY * -1
-                    dx, dy = dx * -1, dy * -1
-            ball_A.velocity_xy = DX, DY
-            ball_B.velocity_xy = dx, dy
-            self.balls[i] = ball_A
-            self.balls[j] = ball_B
+    def new_velocity(self, ball_A, ball_B):
+        x1, y1 = ball_A.center_xy
+        x2, y2 = ball_B.center_xy
+        v1 = ball_A.velocity_xy
+        v2 = ball_B.velocity_xy
+        m1 = 1
+        m2 = 1
+        n = x1 - x2, y1 - y2  # normal vector
+        n_mag = sqrt(n[0] ** 2 + n[1] ** 2)  # magnitude of normal vector
+        un = n[0] / n_mag, n[1] / n_mag  # unit vector of n
+        ut = -1 * un[1], un[0]  # unit tangent vector of n
+        v1n = self.dot_product(un, v1)
+        v1t = self.dot_product(ut, v1)
+        v2n = self.dot_product(un, v2)
+        v2t = self.dot_product(ut, v2)
+        v1t_ = v1t
+        v2t_ = v2t
+        v1n_ = (v1n * (m1 - m2) + 2 * m2 * v2n) / (m1 + m2)
+        v2n_ = (v2n * (m2 - m1) + 2 * m1 * v1n) / (m1 + m2)
+        v1n__ = (un[0] * v1n_, un[1] * v1n_)
+        v1t__ = (ut[0] * v1t_, ut[1] * v1t_)
+        v2n__ = (un[0] * v2n_, un[1] * v2n_)
+        v2t__ = (ut[0] * v2t_, ut[1] * v2t_)
+        _v1_ = v1n__[0] + v1t__[0], v1n__[1] + v1t__[1]
+        _v2_ = v2n__[0] + v2t__[0], v2n__[1] + v2t__[1]
+        return (_v1_, _v2_)
+    def dot_product(self, vector_1, vector_2):
+        x, y = vector_1
+        X, Y = vector_2
+        return x * X + y * Y
     def _ball_ball_collision(self, ball_A, ball_B):
         X, Y = ball_A.center_xy
         R = ball_A.radius_xy[0]
@@ -172,30 +184,38 @@ class Model:
         r = ball_B.radius_xy[0]
         Rr_sum_sqd = (R + r) ** 2
         D_sqd = (X - x) ** 2 + (Y - y) ** 2
-        if D_sqd <= Rr_sum_sqd:
+        if D_sqd < Rr_sum_sqd:
+            last_hit_for_A = '{:2}-{:20}'.format(str(ball_A.last_hit.number), ball_A.last_hit.color)
+            last_hit_for_B = '{:2}-{:20}'.format(str(ball_B.last_hit.number), ball_B.last_hit.color)
+            A = 'A: {:2}-{:20} Last Hit: {} Total Hits: {}'.format(ball_A.number, ball_A.color, last_hit_for_A, ball_A.number_hit)
+            B = 'B: {:2}-{:20} Last Hit: {} Total Hits: {}'.format(ball_B.number, ball_B.color, last_hit_for_B, ball_B.number_hit)
+            print(A, B, sep='\t')
             return True
         else:
             return False
-    def _recent_ball_collision(self, ij):
-        return self._ball_timers_dict[(ij)]
-    def _set_ball_collision_timer_bool_to_True(self, ij):
-        self._ball_timers_dict[(ij)] = True
-    def _set_ball_collision_timer_bool_to_False(self, ij):
-        self._ball_timers_dict[(ij)] = False
-    def _random_direction_change_for_ball_ball_collision(self, DXDYdxdy):
-        DX, DY, dx, dy = DXDYdxdy
-        choices = ['XY', 'XY', 'XY']
-        choice = PICK(choices)
-        if choice == 'X':
-            DX, dx = DX * -1, dx * -1
-            DY, dy = DY,    dy
-        elif choice == 'Y':
-            DX, dx = DX,    dx
-            DY, dy = DY * -1, dy * -1
-        elif choice == 'XY':
-            DX, dx = DX * -1, dx * -1
-            DY, dy = DY * -1, dy * -1
-        return (DX, DY, dx, dy)
+    def _update_last_hits(self, ball_A, ball_B):
+        if ball_A.last_hit == ball_B:
+            ball_A.increment_last_hit()
+        else:
+            ball_A.set_new_last_hit(ball_B)
+        if ball_B.last_hit == ball_A:
+            ball_B.increment_last_hit()
+        else:
+            ball_B.set_new_last_hit(ball_A)
+        return (ball_A, ball_B)
+    def _blacklist(self, ball_A, ball_B):
+        ij = min(ball_A.number, ball_B.number), max(ball_A.number, ball_B.number)
+        if ball_A.number_hit > 10 and ball_B.number_hit > 10:
+            A = '{}-{}'.format(ball_A.number, ball_A.color)
+            B = '{}-{}'.format(ball_B.number, ball_B.color)
+            star = '*' * 20
+            print('{} BREAKING {} and {}!! {}'.format(star, A, B, star))
+            self._set_ball_collision_timer_bool(ij, True)
+            t = Timer(5, partial(self._set_ball_collision_timer_bool, ij, False))
+            t.start()
+        return self._ball_collision_dict[ij]
+    def _set_ball_collision_timer_bool(self, ij, TF):
+        self._ball_collision_dict[(ij)] = TF
 class Ball:
     def __init__(self, number, center, radius, velocity, color):
         self.number = number
@@ -204,6 +224,9 @@ class Ball:
         self.velocity_xy = velocity
         self.color = color
         self.edges = self._calculate_edge_values()
+        self.last_hit = self
+        self.number_hit = 0
+        self.blacklist = []
     def __str__(self):
         n = '{:<2}'.format(self.number)
         number = 'Ball ' + n
@@ -235,6 +258,13 @@ class Ball:
         dx, dy = self.velocity_xy
         self.center_xy = (x + dx, y + dy)
         self.edges = self._calculate_edge_values()
+    def increment_last_hit(self):
+        self.number_hit += 1
+    def set_new_last_hit(self, ball):
+        self.last_hit = ball
+        self.number_hit = 1
+    def blacklist(self, ball):
+        pass
 class Game:
     def __init__(self):
         self.game_model = Model()
